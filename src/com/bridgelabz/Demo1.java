6c046cde4a513e36ea3a0e8193eb885167c37a01
Rampravesh25 <rampraveshmallah@gmail.com>
2022-09-07 22:09:55 +0530

    Day 1 Problems

---
+++ src/com/bridgelabz/Demo1.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class Demo1 {
+    public static void main(String[] args) {
+
+        /* Print the Value*/
+        System.out.println("Enter The Values !!");
+        /*PrimitiveDataType*/
+        int s = 10;
+        /*StringEqual*/
+        String str = "Rampravesh";
+        String str2 = "Rampravesh";
+        System.out.println(str.equals(str2));
+    }
+}
