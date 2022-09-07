40e39039c77414d5fa7131ac528f75bd9a0d7d3f
Rampravesh25 <rampraveshmallah@gmail.com>
2022-09-07 23:08:38 +0530

    Day 1 Problems: 1.7

---
+++ src/com/bridgelabz/Demo3.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+public class Demo3 {
+    public static void main(String[] args) {
+
+        // year to be checked
+        int year = 1900;
+        boolean leap = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) leap = true;
+                else leap = false;
+            } else leap = true;
+        } else leap = false;
+
+        if (leap) System.out.println(year + " is a leap year.");
+        else System.out.println(year + " is not a leap year.");
+    }
+}
+
